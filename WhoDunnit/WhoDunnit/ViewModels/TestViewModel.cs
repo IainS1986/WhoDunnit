c006338a53eda01b95f957bd1150ebf441cadfc9
Iain Stanford <i_stanford@hotmail.com>
2018-01-07 00:10:14 +0000

    Core project now added correctly

---
+++ WhoDunnit/WhoDunnit/ViewModels/TestViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WhoDunnit.ViewModels
+{
+    class TestViewModel : INotifyPropertyChanged
+    {
+        private string m_text = "Test";
+        public string Text
+        {
+            get
+            {
+                return m_text;
+            }
+            set
+            {
+                m_text = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TestViewModel()
+        {
+            Text = "Hello Cluedo!!!";
+        }
+    }
+}
